32b6e9ea0e046ba6088fca27f6cbd4fb2f40d5eb
pedro martingaste <pitermartingaste@gmail.com>
2025-04-12 19:05:33 -0300

    register clg

--- pages/api/register.js
+++ pages/api/register.js
@@ -3,6 +3,7 @@ import { Timestamp } from "firebase-admin/firestore";
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
+  console.log("Valor de FIREBASE_PROJECT_ID en Vercel:", process.env.FIREBASE_PROJECT_ID);
   if (req.method !== "POST") return res.status(405).end();
 
   const { username, email, password } = req.body; // Eliminé country y profession
